c3a5d9413bb98b3f3480348f98f261d40b8e4977
Paweł Potępa <potepapawel@gmail.com@ppotepa.github.com>
2020-11-23 10:00:18 +0100

    Reomove accidentaly copied file

---
+++ Sense.Recruitment.SnakeRoyale.SimpleGameEngine/Services/CustomRenderers/WPFRenderer/WPFRenderer.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+namespace Sense.Recruitment.SnakeRoyale.Engine.Services.CustomRenderers
+{
+    public class WPFRenderer : IRenderer
+    {
+        private readonly Window GameWindow;
+        public void Initialize()
+        {
+            GameWindow.Title = "Snakes WPF Demo";
+            GameWindow.Width = 1000;
+            GameWindow.Height = 1000;
+            //GameWindow.Show();
+        }
+
+        public void Render()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RenderSingleObject(GameObject @object)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
