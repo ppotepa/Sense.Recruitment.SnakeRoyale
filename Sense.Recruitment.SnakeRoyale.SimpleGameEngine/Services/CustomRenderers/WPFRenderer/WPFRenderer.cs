3594edda27e101eab9896e099d1553cc0242aff8
Paweł Potępa <potepapawel@gmail.com@ppotepa.github.com>
2020-11-23 10:01:10 +0100

    Commented Out WPF Renderer

--- Sense.Recruitment.SnakeRoyale.SimpleGameEngine/Services/CustomRenderers/WPFRenderer/WPFRenderer.cs
+++ Sense.Recruitment.SnakeRoyale.SimpleGameEngine/Services/CustomRenderers/WPFRenderer/WPFRenderer.cs
@@ -1,16 +1,28 @@
-﻿using System.Windows;
-
-namespace Sense.Recruitment.SnakeRoyale.Engine.Services.CustomRenderers
+﻿namespace Sense.Recruitment.SnakeRoyale.Engine.Services.CustomRenderers
 {
     public class WPFRenderer : IRenderer
     {
-        private readonly Window GameWindow;
+        //private readonly Window GameWindow;
+        //public void Initialize()
+        //{
+        //    GameWindow.Title = "Snakes WPF Demo";
+        //    GameWindow.Width = 1000;
+        //    GameWindow.Height = 1000;
+        //    //GameWindow.Show();
+        //}
+
+        //public void Render()
+        //{
+        //    throw new System.NotImplementedException();
+        //}
+
+        //public void RenderSingleObject(GameObject @object)
+        //{
+        //    throw new System.NotImplementedException();
+        //}
         public void Initialize()
         {
-            GameWindow.Title = "Snakes WPF Demo";
-            GameWindow.Width = 1000;
-            GameWindow.Height = 1000;
-            //GameWindow.Show();
+            throw new System.NotImplementedException();
         }
 
         public void Render()
